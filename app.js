7f29ee7fb9371c7bf48fda3c1e0a77ddc1b686c3
许轶淼 <xuyimiao@xuyimiaodeMacBook-Pro.local>
2016-08-31 22:06:16 +0800

    initial app

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 var express = require('express');
 var routes = require('/routes');
 var mongo = require('mongo');
+var http = require('http');
+var mongoskin = require('mongoskin');
+var dbUrl = process.env.MONGOHQ_URL || "mongodb://@localhost:27017/simplequery"
+var app = express();
+
